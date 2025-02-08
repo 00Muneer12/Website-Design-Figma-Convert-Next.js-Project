655666ba3651cdddf68dc71d0d10a1f07d48accd
“[firstname <[gmugsk@gmail.com]>
2025-02-08 22:26:28 +0500

    Figma

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,10 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-2 px-4 text-xs">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        
-        {/* Conditions Section
-        <div className="flex flex-col space-y-0.5">
-          <h2 className="text-orange-500 font-semibold"></h2>
-          <Link href={''} className="hover:text-orange-400 transition"></Link>
-          <Link href={''} className="hover:text-orange-400 transition"></Link>
-        </div>
-
-        DivideR
-        <div className="hidden md:block bg-white w-[1px] h-6"></div> */}
-
-        {/* Discount Winners Section
-        <div className="flex flex-col space-y-0.5">
-          <h2 className="text-orange-500 font-semibold"></h2>
-          <Link href={''} className="hover:text-orange-400 transition"></Link>
-          <Link href={''} className="hover:text-orange-400 transition"></Link>
-        </div> */}
 
         {/* Logo Section */}
         <div className=" mt-2 flex">
